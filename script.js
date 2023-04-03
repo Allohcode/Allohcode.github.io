da31cdd2ee7f5c24fc0e33220d2c8395abed8d52
Allohcode <allomelas@gmail.com>
2023-04-03 23:42:46 +0100

    added new effect to index page

---
+++ script.js
@@ -0,0 +1,9 @@
+const banner = document.querySelector(".profile-info");
+
+window.addEventListener("load", () => {
+  setTimeout(() => {
+    banner.classList.add("show-banner");
+    // banner.style.transition = "0.4s";
+  }, 500);
+});
+console.log("melas");
