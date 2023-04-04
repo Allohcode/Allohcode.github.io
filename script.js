924053a0201cc21d3598d1ecd1b652ecea738bb2
Allohcode <allomelas@gmail.com>
2023-04-04 14:06:04 +0100

    i added an intersection observer

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
 const banner = document.querySelector(".profile-info");
 
-window.addEventListener("load", () => {
-  setTimeout(() => {
-    banner.classList.add("show-banner");
-    // banner.style.transition = "0.4s";
-  }, 1000);
+const sections = document.querySelectorAll(".section");
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(
+    (entry) => {
+      entry.target.classList.toggle("section-show", entry.isIntersecting);
+    },
+    {
+      root: null,
+      // rootMargin: "-100px",
+      threshold: 1,
+    }
+  );
 });
-console.log("melas");
+
+sections.forEach((sec) => {
+  observer.observe(sec);
+});
+
+// window.addEventListener("load", () => {
+//   setTimeout(() => {
+//     banner.classList.add("show-banner");
+//     // banner.style.transition = "0.4s";
+//   }, 1000);
+// });
+// console.log("melas");
