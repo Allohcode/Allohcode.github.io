cc2ab7331b66da64cc769c82d3621b7234ba1729
Allohcode <allomelas@gmail.com>
2023-04-06 13:16:59 +0100

    i made the mobile view nav bar

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
+//vataibles decleartion
 const banner = document.querySelector(".profile-info");
-
+const allPprojects = document.querySelectorAll(".allprojects");
 const sections = document.querySelectorAll(".section");
+const bars = document.querySelector(".bars");
+const nav = document.querySelector(".nav-links");
+let click = true;
+// click on nav
 
+bars.addEventListener("click", () => {
+  let cancle = document.querySelector(".fa-times");
+  let bar = document.querySelector(".bars .fa-bars");
+
+  nav.classList.toggle("show-nav");
+  bars.classList.add("bars-rot");
+
+  if (bars && click === true) {
+    click = false;
+    bar.style.display = "none";
+    cancle.style.display = "block";
+    bar.classList.add("bars-rot");
+  } else {
+    click = true;
+    bar.style.display = "block";
+    cancle.style.display = "none";
+    cancle.classList.add("bars-rot");
+  }
+});
+
+//functions
 const observer = new IntersectionObserver((entries) => {
   entries.forEach(
     (entry) => {
